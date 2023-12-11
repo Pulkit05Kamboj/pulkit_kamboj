b4c556b6ea0237d3da50a9b980cfa711cdfdae2d
Pulkit Kamboj <pulkit.kamboj01@student.csulb.edu>
2023-12-12 00:33:14 +0530

    added app

---
+++ app/pulkit_kamboj_CMA/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Contacts(models.Model):
+    contact_name = models.CharField(max_length=100)
+    contact_email = models.CharField(max_length=250)
+    contact_notes = models.CharField(max_length=250)
+    created_time = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.contact_name
